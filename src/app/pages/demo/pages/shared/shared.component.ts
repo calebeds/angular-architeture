dd0d9923e4c8368d050c139c3c46d73b48f52817
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-06-23 17:08:08 -0300

    add form-field validators

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -4,26 +4,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-shared',
   templateUrl: './shared.component.html',
-  styleUrls: ['./shared.component.scss']
+  styleUrls: ['./shared.component.scss'],
 })
 export class SharedComponent implements OnInit {
-
   form!: FormGroup;
+  isInline = false;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.form = this.fb.group({ 
-      input: [null]
+    this.form = this.fb.group({
+      input: [
+        null,
+        {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.minLength(3)],
+        },
+      ],
     });
   }
 
   onPatchValue() {
-    this.form.patchValue({ input: 'test'});
+    this.form.patchValue({ input: 'test' });
   }
 
   onSubmit(): void {
     console.log('Submit');
   }
 
+  onToggleInline(): void {
+    this.isInline = !this.isInline;
+  }
 }
---
+++ src/app/shared/controls/form-field/form-field.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-field',
+  templateUrl: './form-field.component.html',
+  styleUrls: ['./form-field.component.scss'],
+})
+export class FormFieldComponent implements OnInit {
+  @Input()
+  label = '';
+
+  @Input()
+  required = false;
+
+  @Input()
+  isInline = false;
+
+  @Input()
+  control!: AbstractControl;
+
+  constructor() {
+    this.isInline = true;
+  }
+
+  ngOnInit(): void {}
+
+  hasError(): boolean {
+    return this.control && this.control.invalid && this.control.touched;
+  }
+
+  get errorKey(): string {
+    const errorKey =
+      this.control &&
+      this.control.errors &&
+      Object.keys(this.control.errors)[0];
+    return errorKey ? <string>errorKey : '';
+  }
+}
