fcddd384013efa418620d00932743dec640f2651
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-06-26 22:22:54 -0300

    implement autocomplete control on shared

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -40,6 +40,10 @@ export class SharedComponent implements OnInit {
         },
       ],
       password: [null, { updateOn: 'blur', validators: [Validators.required] }],
+      autocomplete: [
+        null,
+        { updateOn: 'change', validators: [Validators.required] },
+      ],
       select: [null, { updateOn: 'change', validators: [Validators.required] }],
       checkboxes: [
         null,
