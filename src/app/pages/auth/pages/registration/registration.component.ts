7bde7167ab28ba5e361f9f3b8c4f618a11a26a72
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-07-01 17:27:03 -0300

    add registration template

--- src/app/pages/auth/pages/registration/registration.component.ts
+++ src/app/pages/auth/pages/registration/registration.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { regex, regexErrors, markFormGroupTouched } from '@app/shared';
+
+import { Store, select } from '@ngrx/store';
+import * as fromRoot from '@app/store';
+import * as fromUser from '@app/store/user';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegistrationComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  regexErrors = regexErrors;
+
+  constructor(private fb: FormBuilder, private store: Store<fromRoot.State>) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group(
+      {
+        email: [
+          null,
+          {
+            updateOn: 'blur',
+            validators: [
+              Validators.required,
+              Validators.maxLength(120),
+              Validators.pattern(regex.email),
+            ],
+          },
+        ],
+        password: [
+          null,
+          {
+            updateOn: 'blur',
+            validators: [
+              Validators.required,
+              Validators.minLength(6),
+              Validators.maxLength(30),
+              Validators.pattern(regex.password),
+            ],
+          },
+        ],
+        passwordRepeat: [
+          null,
+          {
+            updateOn: 'blur',
+            validators: [
+              Validators.required,
+              Validators.minLength(6),
+              Validators.maxLength(30),
+              Validators.pattern(regex.password),
+            ],
+          },
+        ],
+      },
+      { validator: this.repeatPasswordValidator }
+    );
   }
 
+  private repeatPasswordValidator(group: FormGroup): {
+    [key: string]: boolean; //It doesn't make sense
+  } {
+    const password = group.get('password');
+    const passwordRepeat = group.get('passwordRepeat');
+
+    return passwordRepeat?.value && password?.value !== passwordRepeat.value
+      ? { repeat: true }
+      : { repeat: false };
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+    } else {
+      markFormGroupTouched(this.form);
+    }
+  }
 }
--- src/app/pages/auth/pages/registration/registration.module.ts
+++ src/app/pages/auth/pages/registration/registration.module.ts
@@ -4,14 +4,28 @@ import { CommonModule } from '@angular/common';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration.component';
 
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {
+  FormFieldModule,
+  InputModule,
+  PasswordModule,
+} from '@app/shared/controls';
+import { ButtonModule, SpinnerModule } from '@app/shared';
 
 @NgModule({
-  declarations: [
-    RegistrationComponent
-  ],
+  declarations: [RegistrationComponent],
   imports: [
     CommonModule,
-    RegistrationRoutingModule
-  ]
+    RegistrationRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormFieldModule,
+    InputModule,
+    PasswordModule,
+    ButtonModule,
+    SpinnerModule,
+  ],
 })
-export class RegistrationModule { }
+export class RegistrationModule {}
